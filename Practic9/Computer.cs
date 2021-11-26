abc2fbe8688dfc4f99961a2fbae8bce12730a913
исп-31 <-31@LAB17-02>
2021-11-26 16:25:23 +0300

    Сделан конструктор

---
+++ Practic9/Computer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practic9
+{
+
+    struct Computer
+    {
+        string Processor { get; set; }
+        int RAM { get; set; }
+        int Memory { get; set; }
+        string GPU { get; set; }
+
+        public Computer(string processor, int ram, int memory, string gpu)//RAM - оператива. gpu - видеокарта
+        {
+            Processor = processor;
+            RAM = ram;
+            Memory = memory;
+            GPU = gpu;
+        }
+        
+       
+    }
+}
