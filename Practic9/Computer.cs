f7254f543ff948005388ad349bd524b767cdc3d0
Nazarov Dima <dmitriynazarov90@gmail.com>
2021-12-02 22:03:52 +0300

    Программа работает

--- Practic9/Computer.cs
+++ Practic9/Computer.cs
@@ -7,10 +7,10 @@ namespace Practic9
 
     struct Computer
     {
-        string Processor { get; set; }
-        int RAM { get; set; }
-        int Memory { get; set; }
-        string GPU { get; set; }
+        public string Processor { get; set; }
+        public int RAM { get; set; }
+        public int Memory { get; set; }
+        public string GPU { get; set; }
 
         public Computer(string processor, int ram, int memory, string gpu)//RAM - оператива. gpu - видеокарта
         {
--- Practic9/MainWindow.xaml.cs
+++ Practic9/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,23 +17,74 @@ using System.Windows.Shapes;
 namespace Practic9
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// ИСП-31. Назаров Д. Вариант 2. 
+    /// Заполнить таблицу с аппаратными средствами на 5 компьютеров с полями: тип процессор, память, HDD, видео.
+    /// Вывести результат на экран.Вывести средний объем памяти.
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        ObservableCollection<Computer> _pcToAdd = null;
+        Computer _computer;
+        int _averageRam = 0;
+        int _averageMemory = 0;
+         public MainWindow()
         {
             InitializeComponent();
         }
 
         private void help_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("ИСП-31. Назаров Д. Вариант 2. \nЗаполнить таблицу с аппаратными средствами на 5 компьютеров с полями: тип процессор, память, HDD, видео.Вывести результат на экран.Вывести средний объем памяти.", "О программе");
+            MessageBox.Show("ИСП-31. Назаров Д. Вариант 2. \nЗаполнить таблицу с аппаратными средствами на 5 компьютеров с полями: тип процессор, память, HDD, видео.Вывести результат на экран. Вывести средний объем памяти.", "О программе");
         }
 
         private void exit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void addPC_Click(object sender, RoutedEventArgs e)
+        {
+            bool isNotErrorInRam = Int32.TryParse(ramOutput.Text, out int pcRAM);
+            bool isNotErrorInMemory = Int32.TryParse(memoryOutput.Text, out int pcMemory);
+            if (isNotErrorInRam && isNotErrorInMemory)
+            {
+                _computer.Processor = processorOutput.Text;
+                _computer.RAM = pcRAM;
+                _computer.Memory = pcMemory;
+                _computer.GPU = gpuOutput.Text;
+
+                if (_pcToAdd == null)
+                {
+                    _pcToAdd = new ObservableCollection<Computer>();
+                    dataGrid.ItemsSource = _pcToAdd;
+                }
+                _pcToAdd.Add(_computer);
+
+                _averageRam += pcRAM;
+                averageRAM.Text = (_averageRam / _pcToAdd.Count()).ToString();
+                _averageMemory += pcMemory;
+                averageMemory.Text = (_averageMemory / _pcToAdd.Count()).ToString();
+
+                processorOutput.Clear();
+                ramOutput.Clear();
+                memoryOutput.Clear();
+                gpuOutput.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                memoryOutput.Clear();
+                ramOutput.Clear();
+            }
+
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            processorOutput.Clear();
+            ramOutput.Clear();
+            memoryOutput.Clear();
+            gpuOutput.Clear();
         }
     }
 }
